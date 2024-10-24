3f05d9b0d898d5f26572fc51249160aaf6756fc6
Matt <matt.j.zbik@gmail.com>
2024-10-24 14:06:07 -0600

    feat: 5 - Create GET /movies Endpoint. ## Changes - Added additional packages for swagger. - Added endpoint `/movies`. - Added swagger js documentation for endpoint. - Added Swagger endpoint `/docs` for documentation.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
 import express from 'express';
 import { AppDataSource } from './data-source';
+import { moviesRouter } from './routes/movies';
+import { setupSwagger } from './swagger';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
 
 AppDataSource.initialize()
   .then(() => {
@@ -15,9 +15,15 @@ AppDataSource.initialize()
     console.error('Error during Data Source initialization:', err);
   });
 
+app.use(express.json());
+
 app.get('/', (_, res) => {
   res.send('Welcome to the Personal Movie API');
 });
+
+app.use('/', moviesRouter);
+
+setupSwagger(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
