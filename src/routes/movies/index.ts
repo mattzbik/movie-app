48d5e167bc96d4062371445d9e9a2e5e1a1182d9
Matt <matt.j.zbik@gmail.com>
2024-10-25 11:55:16 -0600

    feat: 7 - Get Movie ID ## Changes - Ran into some issues with express@5, downgraded to previous version. - Updated `tsconfig` as `noImplicitReturns` option isn't necessary at this time. - Added `GET` request on returning movie by `id`.

--- src/routes/movies/index.ts
+++ src/routes/movies/index.ts
@@ -83,3 +83,51 @@ moviesRouter.post('/movies', async (req, res, next) => {
     next(err);
   }
 });
+
+/**
+ * @swagger
+ * /movies/{id}:
+ *   get:
+ *     summary: Get a movie by ID
+ *     responses:
+ *       200:
+ *         description: Movie details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   title:
+ *                     type: string
+ *                   release_date:
+ *                     type: string
+ *                     format: date
+ *                   overview:
+ *                     type: string
+ *                   runtime:
+ *                     type: integer
+ *       404:
+ *         description: Movie not found
+ */
+moviesRouter.get('/movies/:id', async (req, res, next) => {
+  try {
+    const movieRepository = AppDataSource.getRepository(Movie);
+    const id = parseInt(req.params.id);
+    const movie = await movieRepository.findOneBy({
+      id,
+    });
+
+    if (!movie) {
+      return res.status(404).json({ error: 'Movie not found' });
+    }
+
+    res.json(movie);
+  } catch (err) {
+    res.status(500).json({ error: err });
+    next(err);
+  }
+});
